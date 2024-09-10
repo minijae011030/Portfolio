7ac97669203bce03aa0a7f3ad6c31d3a985bd962
강민재 <minijae011030@naver.com>
2024-09-11 00:05:21 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
+
 function App() {
   return (
-    <div>
-      <p>app</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
