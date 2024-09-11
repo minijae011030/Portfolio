428fec2ba00b0186839705bef0660a4317e46797
강민재 <minijae011030@naver.com>
2024-09-11 17:51:18 +0900

    import image files to firebase

---
+++ src/Home/component/Petfinity.jsx
@@ -0,0 +1,22 @@
+import data from "../../utils/data/project.json";
+
+function Petfinity() {
+  const project_image = data[1].project_image;
+
+  return project_image.map((project, index) => {
+    return (
+      <div key={index}>
+        <p>{project.name}</p>
+        <p>{project.description}</p>
+        <img
+          src={`${process.env.REACT_APP_FIREBASE}/${project.url}`}
+          key={index}
+          style={{ width: 300 }}
+          alt=""
+        />
+      </div>
+    );
+  });
+}
+
+export default Petfinity;
--- src/Home/component/Projects.jsx
+++ src/Home/component/Projects.jsx
@@ -1,17 +1,16 @@
 import styles from "../styles/Home.module.css";
-import projects from "../../utils/data/project.json";
+import Antiheimer from "./Antiheimer";
+import Archive from "./Archive";
+import Petfinity from "./Petfinity";
 
 function Projects() {
   return (
     <div className={styles.projects}>
       <p>projects</p>
-      {projects.map((project, index) => {
-        return (
-          <div key={index}>
-            <p>{project.project_name}</p>
-          </div>
-        );
-      })}
+      <Archive />
+      <Petfinity />
+      <Antiheimer />
+      <hr />
     </div>
   );
 }
