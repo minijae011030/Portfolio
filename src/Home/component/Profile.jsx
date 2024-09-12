3b4df7ac2a88b6f68401f0695655496c24bc4d2d
강민재 <minijae011030@naver.com>
2024-09-12 20:55:25 +0900

    profile

--- src/Home/component/Profile.jsx
+++ src/Home/component/Profile.jsx
@@ -1,15 +1,55 @@
 import styles from "../styles/Home.module.css";
+import profile_img from "../../utils/image/profile_ian.jpg";
+import { useEffect, useState } from "react";
 
 function Profile() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  function handleResize() {
+    setIsSmallScreen(window.innerWidth <= 768);
+  }
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className={styles.profile}>
-      <p>ABOUT ME</p>
-      <p>강민재</p>
-      <p>{process.env.REACT_APP_PHONE}</p>
-      <p>{process.env.REACT_APP_EMAIL}</p>
-      <a href={process.env.REACT_APP_BLOG}>블로그</a>
-      <a href={process.env.REACT_APP_RESUME}>이력서</a>
-      <a href={process.env.REACT_APP_GITHUB}>깃허브</a>
+      <h1 className={styles.title}>ABOUT ME</h1>
+      <div
+        className={
+          isSmallScreen
+            ? styles.small_screen_outer_info
+            : styles.big_screen_outer_info
+        }
+      >
+        <div
+          className={
+            isSmallScreen ? styles.small_screen_info : styles.big_screen_info
+          }
+        >
+          <div className={styles.profile_img}>
+            <img src={profile_img} alt="profile_img" />
+          </div>
+          <div className={styles.text}>
+            <p className={styles.name}>강민재</p>
+            <p className={styles.university}>Sejong Univ.</p>
+            <p className={styles.email}>{process.env.REACT_APP_EMAIL}</p>
+            <p className={styles.phone}>{process.env.REACT_APP_PHONE}</p>
+          </div>
+        </div>
+        <div
+          className={
+            isSmallScreen ? styles.small_screen_link : styles.big_screen_link
+          }
+        >
+          <a href={process.env.REACT_APP_BLOG}>블로그</a>
+          <a href={process.env.REACT_APP_RESUME}>이력서</a>
+          <a href={process.env.REACT_APP_GITHUB}>깃허브</a>
+        </div>
+      </div>
     </div>
   );
 }
