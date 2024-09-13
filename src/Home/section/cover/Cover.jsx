d680e255508a17a14b03d2a2c0d721b6ac75815c
강민재 <minijae011030@naver.com>
2024-09-13 21:40:25 +0900

    edit cover

--- src/Home/section/cover/Cover.jsx
+++ src/Home/section/cover/Cover.jsx
@@ -5,10 +5,10 @@ import styles from "./styles/cover.module.css";
 function Cover() {
   return (
     <div className={styles.cover}>
+      <p className={styles.description}>2024 Ian's Portfolio</p>
       <div className={styles.parallaxWrapper}>
         <ParallaxText baseVelocity={-3}>FRONTEND DEV</ParallaxText>
       </div>
-
       <div className={styles.parallaxWrapper}>
         <ParallaxText baseVelocity={3}>KANG MINJAE</ParallaxText>
       </div>
